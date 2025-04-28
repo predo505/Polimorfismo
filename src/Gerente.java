9270e6d9cd1bde3a19a4baeade6680234ddec58f
predo505 <pedroesquecido1@gmail.com>
2025-04-28 01:59:57 -0300

    Update Gerente.java

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class Gerente extends Funcionario {
 
-public class Gerente {
+    private double bonusExtra;
+
+    public Gerente() {
+        super();
+        this.bonusExtra = 0.0;
+    }
+
+    public Gerente(String nome, double salario, double bonusExtra) {
+        super(nome, salario);
+        this.bonusExtra = bonusExtra;
+    }
+
+    @Override
+    public double calcularBonus() {
+        return (super.calcularBonus() + bonusExtra);
+    }
+
+    @Override
+    public String exibirDados() {
+        return "Gerente - Nome: " + getNome() + " - Salário: " + getSalario() + " - Bônus: " + calcularBonus();
+    }
 }
