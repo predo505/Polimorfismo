3296e228fd09b5619392d04f5358c7c26fe4c515
predo505 <pedroesquecido1@gmail.com>
2025-04-28 01:59:29 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Funcionario {
+
+    private String nome;
+    private double salario;
+
+    public Funcionario() {
+        this.nome = "Indefinido";
+        this.salario = 0.0;
+    }
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularBonus() {
+        return this.salario * 0.10;
+    }
+
+    public String exibirDados() {
+        return "Funcionário - Nome: " + nome + " - Salário: " + salario + " - Bônus: " + calcularBonus();
+    }
 }
